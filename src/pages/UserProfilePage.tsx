3e3c9eb1484f18e9c29f54d0a3aa3f62cbbb61e7
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-31 18:48:16 -0300

    feat: added toasts notification to update user profile

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -4,9 +4,7 @@ import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 const UserProfilePage = () => {
   const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
-  return (
-    <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />
-  );
+  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
 };
 
 export default UserProfilePage;
