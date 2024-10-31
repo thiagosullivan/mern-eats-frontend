9c0c7f8c0f9374f8256f3d3c6f46db6ba0941c08
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-31 14:38:18 -0300

    feat: verify JWT when singin and singup

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -12,13 +11,14 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallBack = (appState?: AppState, user?: User) => {
-    navigate("/auth-callback")
+  const onRedirectCallBack = () => {
+    navigate("/auth-callback");
   };
 
   return (
@@ -27,6 +27,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallBack}
     >
