ed574ce07d09fd3073ce0ab5070a696942c243d5
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-07 01:29:32 -0300

    feat: added search bar component

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+
+const SearchPage = () => {
+    const { city } = useParams();
+
+  return (
+    <div>
+        User searched for {city}
+    </div>
+  )
+}
+
+export default SearchPage
